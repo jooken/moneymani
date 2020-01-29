224c3cd5ee8ce163a3c32ce0d5ab12e228306d81
jooken <jooken@naver.com>
2020-01-30 02:40:41 +0900

    add exclude parameter in SpringBootApplication annotation
    because of data source configuration,
    first of all, exclude data source configuration.
    <code>
    @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
    </code>

--- moneymani-api/src/main/java/com/moneymani/api/MoneymaniApiApplication.java
+++ moneymani-api/src/main/java/com/moneymani/api/MoneymaniApiApplication.java
@@ -2,8 +2,9 @@ package com.moneymani.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class MoneymaniApiApplication {
 
 	public static void main(String[] args) {
