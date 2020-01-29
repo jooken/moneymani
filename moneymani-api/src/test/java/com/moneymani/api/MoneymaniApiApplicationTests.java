fd51c3f0aaacf2beadf3046d95a119dc0967073d
jooken <jooken@naver.com>
2020-01-30 02:23:35 +0900

    create SpringBootApplication

---
+++ moneymani-api/src/test/java/com/moneymani/api/MoneymaniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneymani.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymaniApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
